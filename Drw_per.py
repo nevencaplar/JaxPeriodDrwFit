470108b09d440e90a345aa8680ef75e4fdb21ca1
Neven Caplar <ncaplar@uw.edu>
2023-08-27 12:46:35 -0700

    ensemble batch works

--- Drw_per.py
+++ Drw_per.py
@@ -63,14 +63,23 @@ if __name__ == '__main__':
                          column_mapper=manual_colmap)
     single_lc = ens.compute("source")[id == 0]
     # comment out line below if trying to run ensamble.batch
-    ens.client.close()
+    # ens.client.close()
     ##########
-
     JaxPeriodDrwFit_instance = JaxPeriodDrwFit.JaxPeriodDrwFit()
+    res = ens.batch(JaxPeriodDrwFit_instance.optimize_map, 't', 'y', 'yerr',
+                    compute=True, meta=None, n_init=100)
+    print(res)
+    ens.client.close()
+    """
+    # https://github.com/nevencaplar/epyc_notebooks/blob/main/tiny_lsst.ipynb
+    # has an example where I managed to run `something'
 
     t = single_lc['t'].values
     y = single_lc['y'].values
     yerr = single_lc['yerr'].values
+
+    # This block shows that the code works on a single lightcurve
+    # And it is faster second time
     t1 = time.time()
     test_single_lc_res = JaxPeriodDrwFit_instance.optimize_map(t, y, yerr, n_init=100)
     t2 = time.time()
@@ -82,12 +91,13 @@ if __name__ == '__main__':
     print(f'Execution time for second run with single lc is {t2 - t1} sec')
     print('Best result is:' + str(test_single_lc_res))
 
+    # ensamble batch does not work
+    # but it works in a notebook?!?
+    # what I thought is the minimal example is here, it is because it is not possible to
+    # pickle jitted funtion
     # https://github.com/google/jax/issues/5043
     # https://jax.readthedocs.io/en/latest/errors.html#jax.errors.ConcretizationTypeError
     pickle.dumps(JaxPeriodDrwFit_instance.optimize_map)
+    """
     print('OK')
 
-    # res = ens.batch(JaxPeriodDrwFit_instance.optimize_map, 'time', 'flux', 'error',
-    #                 compute=True, meta=None, n_init=100)
-    # print(res)
-    # ens.client.close()
